a5bf7467844de864c86560b9b0ba0e2d0505e198
Cleydson <cleydson.cg@live.com>
2023-09-22 16:45:22 -0300

    User CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,38 +5,40 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
-} from '@nestjs/common';
+  Delete, Put, HttpCode, HttpStatus
+} from "@nestjs/common";
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { IsPublic } from "src/auth/decorators/is-public.decorator";
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
+  
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  // @Get()
-  // findAll() {
-  //   return this.userService.findAll();
-  // }
-  //
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.userService.findOne(+id);
-  // }
-  //
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.userService.update(+id, updateUserDto);
-  // }
-  //
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.userService.remove(+id);
-  // }
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: number) {
+    return this.userService.remove(id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from "src/user/dto/update-user.dto";
 
 @Injectable()
 export class UserService {
@@ -16,30 +17,45 @@ export class UserService {
     };
 
     const createdUser = await this.prisma.user.create({ data });
-
-    return {
-      ...createdUser,
-      password: undefined,
-    };
+    delete createdUser.password;
+    return createdUser;
   }
 
   findByEmail(email: string) {
     return this.prisma.user.findUnique({ where: { email } });
   }
 
-  // findAll() {
-  //   return `This action returns all user`;
-  // }
-  //
-  // findOne(id: number) {
-  //   return `This action returns a #${id} user`;
-  // }
-  //
-  // update(id: number, _updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
-  // }
-  //
-  // remove(id: number) {
-  //   return `This action removes a #${id} user`;
-  // }
+  async findAll() {
+    const users = await this.prisma.user.findMany();
+    return users.map((user) => {
+      delete user.password;
+      return user;
+    });
+  }
+
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    delete user.password;
+    return user;
+  }
+
+  async update(id: number, _updateUserDto: UpdateUserDto) {
+    const data: Prisma.UserUpdateInput = {
+      ..._updateUserDto,
+      password: await bcrypt.hash(_updateUserDto.password, 10),
+    };
+
+    const updatedUser = await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+    delete updatedUser.password;
+    return updatedUser;
+  }
+
+  async remove(id: number) {
+    await this.prisma.user.delete({ where: { id } });
+    // delete deletedUser.password;
+    // return deletedUser;
+  }
 }
